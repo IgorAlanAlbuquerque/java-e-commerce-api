03d26f5c09beffadf8ed1840c3cdea81a5976604
Igor Alan Albuquerque de Sousa <50077829+IgorAlanAlbuquerque@users.noreply.github.com>
2025-04-23 20:59:46 -0300

    Delete backend/src/main/java/com/igor/ecommerce/service directory

--- backend/src/main/java/com/igor/ecommerce/service/CheckoutService.java
+++
@@ -1,9 +0,0 @@
-package com.igor.ecommerce.service;
-
-import com.igor.ecommerce.dto.Purchase;
-import com.igor.ecommerce.dto.PurchaseResponse;
-
-public interface CheckoutService {
-
-    PurchaseResponse placeOrder(Purchase purchase);
-}
